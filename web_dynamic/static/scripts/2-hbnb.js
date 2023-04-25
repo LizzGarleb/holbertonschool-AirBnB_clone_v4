6639798a99327813a698665f85b36d6fdd482726
LizzGarleb <redscorpion3108@gmail.com>
2023-04-25 16:33:14 -0400

    task 3 done

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,33 +1,34 @@
-$(() => {
-    const amenityDict = {};
-    $(':checkbox').change(() => {
-        if (this.checked) {
-            amenityDict[$(this).data('id')] = $(this).data('name');
-        } else {
-            delete amenityDict[$(this).data('id')];
-        }
-        if ($.isEmptyObject(amenityDict)) {
-            $('.amenities h4').html('&nbsp');
-        } else {
-            $('.amenities h4').text(Object.values(amenityDict).join(', '));
-        }
-    });
+#!/usr/bin/node
+$(document).ready(function () {
+  const amenityIds = {};
+
+  $('input[type=checkbox]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).prop('checked')) {
+      amenityIds[amenityId] = amenityName;
+    } else {
+      delete amenityIds[amenityId];
+    }
+
+    $('.amenities h4').text(Object.values(amenityIds).join(', '));
+  });
 });
 
 $(() => {
-    $.ajax({
-        type: 'GET',
-        url: 'http://0.0.0.0:5001/api/v1/status/',
-        success: (data) => {
-            if (data.status === 'OK') {
-                $('div#api_status').addClass('available');
-            } else {
-                $('div#api_status').removeClass('available');
-            }
-        },
-        error: () => {
-            $('div#api_status').removeClass('available');
-        }
-    });
+  $.ajax({
+    type: 'GET',
+    url: 'http://0.0.0.0:6050/api/v1/status/',
+    success: (data) => {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    },
+    error: () => {
+      $('div#api_status').removeClass('available');
+    }
+  });
 });
-
